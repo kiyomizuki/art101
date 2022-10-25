3bec5026c1d0575a263a313bcb8fa93a639d5373
kiyomizuki <114502886+kiyomizuki@users.noreply.github.com>
2022-10-25 08:31:31 -0700

    Create lab7.js

---
+++ lab7/js/lab7.js
@@ -0,0 +1,20 @@
+// I don't know what I am doing :(
+
+function sortUsername()
+  var userName = window.prompt{"Hi, please tell me your name so I can fix it."};
+  console.log ("userName =", userName);
+  // spit string to array...?
+  var nameArray = userName.split('');
+  console.log ("nameArray =", nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort('');
+  console.log ("nameSort =", nameSort);
+  //what
+  var nameSorted = nameArraySort.join('');
+  console.log ("nameSorted =", nameSorted);
+  // so confused...
+  return nameSorted
+
+// output
+document.WriteIn("Oh hey I fixed your name:",
+  sortUserName(), "</br>");
