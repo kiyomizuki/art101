f84b09e2feca415cd62110761b3dd5c400cc2758
kiyomizuki <114502886+kiyomizuki@users.noreply.github.com>
2022-10-28 13:01:29 -0700

    Update lab.js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,1 +1,35 @@
-???
+/**
+ * Authors: Kiyo Leong <maaleong@ucsc.edu>
+            Ramola Datta <rsdatta@ucsc.edu>
+ * Created: 10.27.2022
+ *
+ **/
+
+//subtraction <- what we wanna do
+//og numbers: 50, 25, 44, 3025, 67, 94
+//equation is x - 5 == (look up what to put here) <- this is the function return
+//function subtract(x) {var results = x - 5 == 0 (doubke check this)}
+
+/*
+
+var subtractTest = [50, 25, 44, 3025, 67, 94];
+
+function mySubtraction(p1, p2){
+                    return x - 5;
+}
+
+*/
+
+//what works so far:
+/*
+var subtractTest = [50, 25, 44, 3025, 67, 94];
+
+function mySubtraction(p1, p2){
+                    return x - 5;
+}
+
+something is missing here ^^ what is it??
+*/
+
+
+//"fruits" of my labor 😞
